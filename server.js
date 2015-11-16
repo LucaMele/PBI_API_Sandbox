07f334d4908b81f520fb8418b97a9d9cff6d8db8
Luca Mele <luca.mele@ctp.com>
2015-11-16 13:33:54 +0100

    make server run 2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('./public'));
 
 
 app.use(bodyParser.urlencoded());
